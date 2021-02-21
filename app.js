18cfbeec2822099a6e9debf6bfad03099eea7397
serity93 <serity93@gmail.com>
2021-02-21 01:24:54 -0800

    Implement basic command handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const { prefix } = require('./config/bot-config.json');
 
 /*
 A Discord bot written specifically for the EXID Discord server.
@@ -11,9 +13,37 @@ Bot owner & main contributor:
 dotenv.config();
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+}
 
 client.once('ready', () => {
   console.log('Ready!');
+});
+
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const commandArgs = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = commandArgs.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  try {
+    command.execute(message, commandArgs);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
 });
 
 client.login(process.env.TEST_TOKEN);
